45dde1f6f5138d224a73f76ab789de58df4a7ccd
Melody Forest <mforest@gsu.edu>
2023-03-07 14:23:06 -0500

    Upstream changes from `gsu-dle/slim-template`

--- public/index.php
+++ public/index.php
@@ -3,32 +3,39 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder      as DIContainerBuilder;
-use GAState\Web\LTI\Slim\App as LTIApp;
-use GAState\Web\LTI\Slim\Env as LTIEnv;
+use GAState\MyLTIApp\Env     as Env;
+use GAState\Web\LTI\Slim\App as BaseApp;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 ob_start();
 
-LTIEnv::load(
-    $_ENV['BASE_DIR'] ?? __DIR__ . '/../',
-    $_ENV['BASE_URI'] ?? dirname($_SERVER['SCRIPT_NAME'])
-);
+(function () {
+    Env::load(
+        strval(realpath(Env::getString(Env::BASE_DIR, __DIR__ . '/../'))),
+        Env::getString(Env::BASE_URI, dirname(dirname($_SERVER['SCRIPT_NAME'])))
+    );
 
-$cmplDir = LTIEnv::getString(LTIEnv::DI_CMPL_DIR);
-$proxyDir = LTIEnv::getString(LTIEnv::DI_PRXY_DIR);
-$defFile = LTIEnv::getString(LTIEnv::DI_DEF_FILE);
+    $containerBuilder = (new DIContainerBuilder())
+        ->useAttributes(true)
+        ->addDefinitions(Env::getString(Env::DI_DEF_FILE));
 
-$container = (new DIContainerBuilder())
-    //->enableCompilation($cmplDir)
-    //->writeProxiesToFile(true, $proxyDir)
-    ->useAttributes(true)
-    ->addDefinitions($defFile)
-    ->build();
+    if (Env::getBool(Env::DI_ENABLE_CMPL)) {
+        $containerBuilder = $containerBuilder
+            ->enableCompilation(Env::getString(Env::DI_CMPL_DIR))
+            ->writeProxiesToFile(true, Env::getString(Env::DI_PRXY_DIR));
+    }
 
-$app = $container->get(LTIApp::class);
-if ($app instanceof LTIApp) {
+    $app = $containerBuilder
+        ->build()
+        ->get(BaseApp::class);
+
+    // This should never happen but... let's handle anyway
+    if (!$app instanceof BaseApp) {
+        throw new RuntimeException("\$app is not an instance of " . BaseApp::class);
+    }
+
     $app->run();
-}
+})();
 
 ob_end_flush();
--- src/Dependencies.php
+++ src/Dependencies.php
@@ -4,27 +4,50 @@ declare(strict_types=1);
 
 namespace GAState\MyLTIApp;
 
-use GAState\MyLTIApp\App     as MyLTIApp;
-use GAState\Web\LTI\Slim\Env as LTIEnv;
-use GAState\Web\LTI\Slim\App as LTIApp;
+use GAState\MyLTIApp\App     as App;
+use GAState\MyLTIApp\Env     as Env;
+use GAState\Web\LTI\Slim\App as BaseApp;
+
+// use GAState\Web\Slim\Cache\AppCacheFactoryInterface as AppCacheFactory;
+// use GAState\Web\Slim\Cache\DBAppCacheFactory        as DBAppCacheFactory;
+// use GAState\Web\Slim\Log\DBLoggerFactory            as DBLoggerFactory;
+// use GAState\Web\Slim\Log\LoggerFactoryInterface     as LoggerFactory;
 
 return (function () {
     /**
-     * Default dependencies from lti-1.3-slim
+     * Default dependencies
      *
-     * @var array<string,mixed> $ltiDeps
+     * @var array<string,mixed> $defaultDeps
      */
-    $ltiDeps = require(LTIEnv::getString(LTIEnv::LTI_DIR) . "/Dependencies.php");
-
+    $defaultDeps = require(Env::getString(Env::LTI_DIR) . "/Dependencies.php");
 
     /**
-     * App dependencies / lti-1.3-slim overrides
+     * Environment variables
+     *
+     * @var array<string,mixed> $envVars
+     */
+    $envVars = [
+        // 'appCacheOptions' => [
+        //     'db_table' => 'AppCache'
+        // ],
+        // 'logTable' => 'AppLog',
+    ];
+
+    /**
+     * App dependencies
      *
      * @var array<string,mixed> $appDeps
      */
     $appDeps = [
-        LTIApp::class => \DI\get(MyLTIApp::class)
+        BaseApp::class => \DI\autowire(App::class),
+
+        // AppCacheFactory::class => \DI\autowire(DBAppCacheFactory::class)
+        //     ->constructorParameter('options', \DI\get('appCacheOptions')),
+        // LoggerFactory::class => \DI\autowire(DBLoggerFactory::class)
+        //     ->constructorParameter('logName', \DI\get('logName'))
+        //     ->constructorParameter('logTable', \DI\get('logTable'))
+        //     ->constructorParameter('logLevel', \DI\get('logLevel')),
     ];
 
-    return array_merge($ltiDeps, $appDeps);
+    return array_merge($defaultDeps, $envVars, $appDeps);
 })();
